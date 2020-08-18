41ecdce3cf855d16df7b0960da3fa3925337a422
HoodedRonzo20 <manuel.ronzoni20@gmail.com>
2020-08-18 20:07:22 +0200

    Index buggato

--- js/utility.js
+++ js/utility.js
@@ -1,11 +1,11 @@
-    $(window).scroll(function() {
-            if($(window).scrollTop() == $(document).height() - $(window).height()) {
-                $.ajax({
-                    url: "/loadmore/",
-                    type: "POST",
-                    success: function(resp){
-                        $('div#response').append(resp);
-                    }
-                });
-            };
-    });
+    // $(window).scroll(function() {
+    //         if($(window).scrollTop() == $(document).height() - $(window).height()) {
+    //             $.ajax({
+    //                 url: "/loadmore/",
+    //                 type: "POST",
+    //                 success: function(resp){
+    //                     $('div#response').append(resp);
+    //                 }
+    //             });
+    //         };
+    // });
