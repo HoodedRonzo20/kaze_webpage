0d92f2ff816cc480d2113a1ea5fd8667678169e4
HoodedRonzo20 <manuel.ronzoni20@gmail.com>
2020-08-22 13:10:23 +0200

    Gestione Uris a seconda del file in implementazione

--- KazeWebPage/Models/HtmlBuilder.js
+++ KazeWebPage/Models/HtmlBuilder.js
@@ -17,11 +17,38 @@ export default class HtmlBuilder
         html = HtmlBuilder.RepleaceKey(html, "tags", post.tags);
         html = HtmlBuilder.RepleaceKey(html, "isAdultContent", post.isAdultContent);
         html = HtmlBuilder.RepleaceKey(html, "dateCreated", post.dateCreated);
-        html = HtmlBuilder.RepleaceKey(html, "uris", post.uris);
+        html = HtmlBuilder.UrisManagement(post.uris[0]);
         html = HtmlBuilder.RepleaceKey(html, "description", post.description);
         html = HtmlBuilder.RepleaceKey(html, "nComments", post.nComments);
+        HtmlBuilder.UrisManagement(html);
         return html;
     }
+
+    static async UrisManagement(uri) {
+        let html = "";
+        let array = uri.split(".");
+        let pathUris = this.pathTemplate + "/Uris_view/";
+        switch(array[array.length-1]) {
+            case "png":
+            case "jpeg":
+            case "jpg":
+                let path = pathUris + "uri_img" + ".html";
+                html = await HtmlBuilder.GetTextFromFile(path);
+                html = HtmlBuilder.RepleaceKey(html, "uri_img", uri);
+                break;
+            case "mp4":
+                let path = pathUris + "uri_vid" + ".html";
+                html = await HtmlBuilder.GetTextFromFile(path);
+                html = HtmlBuilder.RepleaceKey(html, "uri_vid", uri);
+                break;
+            default:
+                let path = pathUris + "uri_file" + ".html";
+                html = await HtmlBuilder.GetTextFromFile(path);
+                html = HtmlBuilder.RepleaceKey(html, "uri_file", uri);
+        } 
+        return html;
+    }
+
     async CreateCommentView(comment) 
     {
         let path = this.pathTemplate + Comment.name + ".html";
