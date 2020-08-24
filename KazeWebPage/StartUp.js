0bbcaa381d78eea5a9200b8c467dba848b6d740d
HoodedRonzo20 <manuel.ronzoni20@gmail.com>
2020-08-24 12:26:00 +0200

    Aggiunto animazione, evento di loadmore on scroll risolti bug

--- KazeWebPage/StartUp.js
+++ KazeWebPage/StartUp.js
@@ -3,6 +3,9 @@ import HtmlBuilder from './Models/HtmlBuilder.js';
 import IndexInjector from './Models/IndexInjector.js';
 import Post from './ViewModel/Post.js';
 //Creazione dei componenti necessari
+$( document ).ready(function() {
+    AOS.init();
+});
 AOS.init();
 var getterJson = new GetterJson("https://localhost:5001", null);
 var htmlBuilder = new HtmlBuilder("./KazeWebPage/View");
@@ -10,28 +13,45 @@ var html = "";
 var ObjPostList = []; //Lista di oggetti Post
 var ObjCommentList = []; //Lista di oggetti Post
 //GetIsAdultContent() Asecconda se true o false mostra i mpost per adulti o pure no sulle cbhiamate normali
-function GetIsAdultContent() {return document.getElementById("isSoloAdultContentCheck").checked == false;}
+function GetIsAdultContent() {return false;}
 
 //Asseconda se isSoloAdultContent e true o false avvia usa le apposite chiamate.
 function GetIsSoloAdultContent() {return document.getElementById("isSoloAdultContentCheck").checked;}
 
-AOS.init();
+Home();
 
 //EventList
 document.getElementById("btnHome").addEventListener("click", Home);
-document.getElementById("PostsContainer").addEventListener("scroll", LoadMorePost);
+document.getElementById("isSoloAdultContentCheck").addEventListener("click", Home);
+
+
+
+    //CHIAMATA GETOLDPOST X I POST PIU VECCHI    
+    $(window).scroll(function() {
+        if($(window).scrollTop() == $(document).height() - $(window).height()) {
+            if( GetIsSoloAdultContent() && ObjPostList[ObjPostList.length-1].id > 3) {
+                console.log(GetIsSoloAdultContent());
+                LoadMorePost();
+            }
+            else if(!GetIsSoloAdultContent() && ObjPostList[ObjPostList.length-1].id > 1) {
+                LoadMorePost();
+            }
+        };
+    });
+
 
 async function Home() {
     if(GetIsSoloAdultContent()) {
         //#region GetNewPostsAdult
         html = "";
+        ObjPostList = [];
         let posts = await getterJson.GetNewPostsAdult();
         if (posts.length > 0) {
             for (let i = 0; i < posts.length; i++) {
                 ObjPostList.push(await Post.CreatePostFromJson(posts[i]));
                 html += await htmlBuilder.CreatePostView(ObjPostList[ObjPostList.length-1]);
             }
-            IndexInjector.InjecHtmlElement("PostsContainer", html);
+            IndexInjector.ReplaceHtmlElement("PostsContainer", html);
         }
         //#endregion GetNewPostsAdult
     } else {
@@ -51,6 +71,7 @@ async function Home() {
 }
 
 async function LoadMorePost() {
+    console.log("SCROLL!!!!!!!!!!!!!");
     if(ObjPostList.length > 0){
         if(GetIsSoloAdultContent()) {
             //#region GetOldPostsAdult
@@ -77,5 +98,5 @@ async function LoadMorePost() {
             IndexInjector.InjecHtmlElement("PostsContainer", html);
             //#endregion GetOldPosts
         }
-    } else { console.log(`La lista di ObjPostList contiene ${ObjPostList.length} post. Per tanto non puo caricare post precendenti`); }
+    } else { console.log(`La lista di ObjPostList contiene ${ObjPostList.length} post, per tanto non puo caricare post precendenti`); }
 }
